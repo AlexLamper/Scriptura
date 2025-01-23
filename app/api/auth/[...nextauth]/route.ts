5aa41362f024f9d77ec40263aafd7ed72bd96855
Alex <lamp0020@hz.nl>
2025-01-23 16:57:57 +0100

    Updated Auth route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,19 +2,17 @@ import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-export const authOptions = {
-    providers: [
-        Github({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        }),
-        Google({
-            clientId: process.env.GOOGLE_ID ?? "",
-            clientSecret: process.env.GOOGLE_SECRET ?? "",
-        }),
-    ],
-};
-
-export const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+  ],
+});
 
 export { handler as GET, handler as POST };
